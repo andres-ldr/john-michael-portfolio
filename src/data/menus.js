1d1c5ea8269a4f3b59d56321bc66d2b0bea42374
Andres <lopez.andresrigoberto@gmail.com>
2025-03-23 16:53:58 -0700

    feat: add new website style

--- src/data/menus.js
+++ src/data/menus.js
@@ -1,60 +1,51 @@
 export const menuMain = [
-    {
-        name: "Home",
-        url: "/",
-        highlighted: false,
-    },
-    {
-        name: "Menu",
-        url: "/menu",
-        highlighted: false,
-    },
-    {
-        name: "About Us",
-        url: "/about-us",
-        highlighted: false,
-    },
-    {
-        name: "Gallery",
-        url: "/gallery",
-        highlighted: false,
-    },
-    {
-        name: "Contact",
-        url: "/contact",
-        highlighted: false,
-    },
-    {
-        name: "Book",
-        url: "/book",
-        highlighted: true,
-    },
+  {
+    name: 'Home',
+    url: '/',
+    highlighted: false,
+  },
+  {
+    name: 'About Me',
+    url: '#about-me',
+    highlighted: false,
+  },
+  {
+    name: 'Services',
+    url: '#services',
+    highlighted: false,
+  },
+  {
+    name: 'Contact',
+    url: '#contact',
+    highlighted: false,
+  },
+  // {
+  //     name: "Book",
+  //     url: "/book",
+  //     highlighted: true,
+  // },
 ];
 
 export const footerPrimary = [
-    {
-        title: "Navigatie",
-        items: [
-            { name: "home", url: "/" },
-        ],
-    },
+  {
+    title: 'Navigatie',
+    items: [{ name: 'home', url: '/' }],
+  },
 ];
 
 export const footerSecondary = [
-    {
-        title: "support",
-        items: [
-            { name: "pricing", url: "/pricing" },
-        ],
-    },
+  {
+    title: 'support',
+    items: [{ name: 'pricing', url: '/pricing' }],
+  },
 ];
 
 export const footerTertiary = [
-    {
-        title: "Links",
-        items: [
-            { name: "privacybeleid", url: '/privacy-policy' },
-            { name: "api status", url: "/api-status" },
-        ],
-    },
+  {
+    title: 'Links',
+    items: [
+      { name: 'privacybeleid', url: '/privacy-policy' },
+      { name: 'api status', url: '/api-status' },
+    ],
+  },
 ];
